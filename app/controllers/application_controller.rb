c207bcc07fc0883a94bc806385bd7d160af30f44
Jamie <jcerexhe@gmail.com>
2016-03-03 16:24:24 +1100

    Added force_non_ssl concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Concerns::ForceNonSSL
+  force_non_ssl
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  force_non_ssl
+  protected
 
-    protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email,:password,:admin) } 
-    end
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email,:password,:admin) } 
+  end
 end
