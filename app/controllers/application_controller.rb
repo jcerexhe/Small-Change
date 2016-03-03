8cce78675e87003a8a21ecb1a02c71f03aaad92e
Jamie <jcerexhe@gmail.com>
2016-03-03 14:45:42 +1100

    Removed ssl config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,16 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_filter do
-    if request.ssl? && Rails.env.production? && controller_name != 'donations'
-      redirect_to :protocol => 'http://', :status => :moved_permanently
-    end
-  end
+  # before_filter do
+  #   if request.ssl? && Rails.env.production? && controller_name != 'donations'
+  #     redirect_to :protocol => 'http://', :status => :moved_permanently
+  #   end
+  # end
 
     protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email,:password,:admin) } 
-
     end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
