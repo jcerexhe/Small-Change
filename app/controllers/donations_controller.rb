7d6c0ab2a9ce33f0504e99c7fb9ec3641b0ea7fe
Jamie <jcerexhe@gmail.com>
2016-03-04 14:01:47 +1100

    Remove CSRF token

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,6 @@
 class DonationsController < ApplicationController
   before_action :set_donation, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :create
   # if Rails.env.production?
   #   force_ssl :only => [:new, :create]
   # end
