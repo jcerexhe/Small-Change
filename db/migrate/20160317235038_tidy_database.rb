d9447642914feea63581ab4b90c42fc5666a1d3d
Jamie <jcerexhe@gmail.com>
2016-03-18 12:35:06 +1100

    Cascading table drops

--- db/migrate/20160317235038_tidy_database.rb
+++ db/migrate/20160317235038_tidy_database.rb
@@ -1,14 +1,14 @@
 class TidyDatabase < ActiveRecord::Migration
   def change
-    drop_table :user_submissions
-    drop_table :profiles
-    drop_table :enquiries
-    drop_table :charity_causes
-    drop_table :causes
-    drop_table :beta_users
+    drop_table :user_submissions, force: :cascade
+    drop_table :profiles, force: :cascade
+    drop_table :enquiries, force: :cascade
+    drop_table :charity_causes, force: :cascade
+    drop_table :causes, force: :cascade
+    drop_table :beta_users, force: :cascade
 
     remove_column :charities, :cause_id, :integer
     add_reference :submissions, :charity, index: true, foreign_key: true
-    add_reference :submissions, :submission, index: true, foreign_key: true 
+    add_reference :submissions, :submission, index: true, foreign_key: true
   end
 end
