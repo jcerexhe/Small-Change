b2f285125f4a16a7f46e7d5dd75925e3c9adea57
Sam Henderson <hendersonsam456@gmail.com>
2016-03-29 17:20:54 +1100

    deleted mail me from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329041218) do
+ActiveRecord::Schema.define(version: 20160329061716) do
 
   create_table "charities", force: :cascade do |t|
     t.string   "name"
@@ -120,7 +120,6 @@ ActiveRecord::Schema.define(version: 20160329041218) do
     t.string   "last_name"
     t.string   "mobile"
     t.boolean  "terms_of_service"
-    t.boolean  "mailing_list"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
