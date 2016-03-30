1654bcfe9883a05de8d7380092e13a45bf5212ca
Sam Henderson <hendersonsam456@gmail.com>
2016-03-30 12:09:31 +1100

    Changed migration2

--- db/migrate/20160323032824_remove_cols_from_submission.rb
+++ db/migrate/20160323032824_remove_cols_from_submission.rb
@@ -2,6 +2,6 @@ class RemoveColsFromSubmission < ActiveRecord::Migration
   def change
     remove_column :submissions, :charity_link, :string
     # remove_column :submissions, :charity_id, :integer
-    remove_reference :submissions, :charity
+    # remove_reference :submissions, :charity
   end
 end
--- db/migrate/20160323033835_remove_charity_from_submission.rb
+++ db/migrate/20160323033835_remove_charity_from_submission.rb
@@ -1,5 +1,5 @@
 class RemoveCharityFromSubmission < ActiveRecord::Migration
   def change
-    remove_column :submissions, :charity, :integer
+    # remove_column :submissions, :charity, :integer
   end
 end
