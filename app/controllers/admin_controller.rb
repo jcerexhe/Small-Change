c3522d0c2e196e3f7eaf30f0be27ededc64f6a9c
Sam Henderson <hendersonsam456@gmail.com>
2016-03-31 11:41:53 +1100

    sum views for admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,10 +8,17 @@ class AdminController < ApplicationController
     @donation_total = Donation.all.sum(:amount).to_i
     @most_actioned_submissions = Submission.link_clicks_desc
     @most_recent_submissions = Submission.most_recent
+
     if params[:most_recent]
       @submissions = @most_recent_submissions
     else
       @submissions = @most_actioned_submissions
     end
+  
+    count = 0
+    @submissions.each do |submission|
+      count = count + submission.impressionist_count
+    end
+    @views = count
   end
 end
