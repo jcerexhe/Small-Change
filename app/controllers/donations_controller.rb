87f364923e41097dfd66e77421eda0ef5cbf811a
Jamie <jcerexhe@gmail.com>
2016-04-11 11:44:54 +1000

    Payment fixes

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -46,6 +46,6 @@ class DonationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def donation_params
-      params.require(:donation).permit(:amount, :charity_id, :user_id, :submission_id)
+      params.require(:donation).permit(:amount, :charity_id, :user_id, :submission_id, :email)
     end
 end
