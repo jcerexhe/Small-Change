84ea68c23dac651635ec4e967ff364c77f0c4250
Sam Henderson <hendersonsam456@gmail.com>
2016-04-15 18:13:18 +1000

    fixed Charity.find_by_charity_id bug

--- app/mailers/donations_mailer.rb
+++ app/mailers/donations_mailer.rb
@@ -2,7 +2,7 @@ class DonationsMailer < ApplicationMailer
 
   def donation_confirmation(donation_id)
     @donation = Donation.find(donation_id)
-    @charity = Charity.find_by_charity_id(@donation.charity_id)
+    @charity = Charity.find(@donation.charity_id)
     @user_email = @donation.user_id ? @donation.user.email : @donation.email
     mail to: @user_email, subject: "Thank you for your donation to <%= @charity.name %>"
   end
