4e62ecbd4e6c7eac40005d694834ef4e218f5ffd
Sam Henderson <hendersonsam456@gmail.com>
2016-04-15 17:20:09 +1000

    changes to the donation email

--- app/mailers/donations_mailer.rb
+++ app/mailers/donations_mailer.rb
@@ -2,7 +2,8 @@ class DonationsMailer < ApplicationMailer
 
   def donation_confirmation(donation_id)
     @donation = Donation.find(donation_id)
+    @charity = Charity.find_by_charity_id(@donation.charity_id)
     @user_email = @donation.user_id ? @donation.user.email : @donation.email
-    mail to: @user_email, subject: "Thank you for your donation"
+    mail to: @user_email, subject: "Thank you for your donation to <%= @charity.name %>"
   end
 end
