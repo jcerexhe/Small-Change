91ba8559018406fdce56c23757fb591c9d1b5443
Jamie <jcerexhe@gmail.com>
2016-04-20 11:50:54 +1000

    Changed SMTP credentials, added devise :confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates :terms_of_service, acceptance: { accept: true }
 
   has_many :submissions
   has_many :donations
 
-  after_create :send_welcome_email, :add_existing_donations
+  after_create :add_existing_donations
 
-  def send_welcome_email
-    UserMailer.signup(self.id).deliver
-  end
+  # def send_welcome_email
+  #   UserMailer.signup(self.id).deliver
+  # end
 
   def add_existing_donations
     Donation.where(email: self.email).each do |donation|
